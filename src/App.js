fb36711b848f85539c1ab99258eacdb94e24667b
fatima-129 <fatimahusnain129@gmail.com>
2023-10-02 00:11:08 +0500

    add styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [product, setProduct] = useState("");
@@ -23,29 +24,47 @@ const App = () => {
   const balance = income + expense;
 
   return (
-    <div>
-      <h1>Expense tracker</h1>
-      <p>Total BALANCE:{balance}</p>
-      <p>INCOME:{income}</p>
-      <p>EXPENSE:{expense}</p>
+    <div className="container">
+      <h1>EXPENSE TRACKER</h1>
+      <div className="balance">
+        <p>Total BALANCE: {balance}</p>
+      </div>
+      <div className="box">
+        <p>INCOME: {income}</p>
+        <div className="line"></div>
+        <p>EXPENSE: {expense}</p>
+      </div>
+      <div className="history">
+        <h1>History</h1>
+        <hr />
+        {history.map((item, idx) => (
+          <div key={idx} className="history_item">
+            <p>{item.product}</p>
+            <p>{item.price}</p>
+          </div>
+        ))}
+      </div>
+
       <form onSubmit={handleTranction}>
-        <input
-          type="text"
-          value={product}
-          onChange={(e) => setProduct(e.target.value)}
-        />
-        <input
-          type="number"
-          value={price}
-          onChange={(e) => setPrice(Number(e.target.value))}
-        />
-        <button type="submit">ADD TRANSACTION</button>
+        <div className="input_container">
+          <input
+            placeholder="Enter product"
+            type="text"
+            value={product}
+            onChange={(e) => setProduct(e.target.value)}
+          />
+          <input
+            placeholder="Enter price"
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <button type="submit" className="btn">
+          ADD TRANSACTION
+        </button>
       </form>
-      {history.map((item, idx) => (
-        <p key={idx}>
-          {item.product}:{item.price}
-        </p>
-      ))}
     </div>
   );
 };
